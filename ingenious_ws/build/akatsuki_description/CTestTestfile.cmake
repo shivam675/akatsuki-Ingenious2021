acb3f25595ad8a32adf613b74e992c77ebde596f
shivam675 <shivam31199@gmail.com>
2021-05-08 00:25:59 +0530

    model made for compi

---
+++ ingenious_ws/build/akatsuki_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/Ingenious/ingenious_ws/src/akatsuki_description
+# Build directory: /home/ros/Ingenious/ingenious_ws/build/akatsuki_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
