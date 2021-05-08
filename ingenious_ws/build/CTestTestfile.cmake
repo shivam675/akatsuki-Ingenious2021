47a844e7d42b1c0980535f772e07964156c0fe9b
shivam675 <shivam31199@gmail.com>
2021-05-08 18:19:43 +0530

    deleted build and cmake files

--- ingenious_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/Ingenious/ingenious_ws/src
-# Build directory: /home/ros/Ingenious/ingenious_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("akatsuki_description")
--- ingenious_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ros/Ingenious/ingenious_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ros/arm_ws/devel;/home/ros/eyrc_ws/devel;/opt/ros/melodic'.split(';') if '/home/ros/arm_ws/devel;/home/ros/eyrc_ws/devel;/opt/ros/melodic' != '' else []
